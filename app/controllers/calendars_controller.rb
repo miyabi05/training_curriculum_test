5bd18b261daf55a1e53877041bbcf82eb75fd05d
miyabi05 <miyabi.m.0511@gmail.com>
2022-12-12 21:02:53 +0900

    Issue1

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -36,7 +36,7 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
+      days = { :month: (@todays_date + x).month, :date: (@todays_date+x).day, :plans: today_plans}
       @week_days.push(days)
     end
     wday_num = Date.today.wday + x　今日の曜日のwdayを呼び出す。timesメソッドのxと連結
